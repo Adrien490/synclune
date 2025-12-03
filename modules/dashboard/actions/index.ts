bc228212b7598533eb01282d1f1535e854fad505
Adrien490 <106741993+Adrien490@users.noreply.github.com>
2025-12-03 09:24:28 +0100

    Delete index.ts

--- modules/dashboard/actions/index.ts
+++
@@ -1,3 +0,0 @@
-export { contactAdrien } from "./contact-adrien";
-export { setContactAdrienVisibility } from "./set-contact-adrien-visibility";
-export { toggleContactAdrienVisibility } from "./toggle-contact-adrien-visibility";
