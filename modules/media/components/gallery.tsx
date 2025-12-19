ac14990a888d2a925e9a253916edf7701f3401c5
Adrien490 <106741993+Adrien490@users.noreply.github.com>
2025-12-19 20:04:41 +0100

    Update gallery.tsx

--- modules/media/components/gallery.tsx
+++ modules/media/components/gallery.tsx
@@ -292,7 +292,7 @@ function GalleryContent({ product, title }: GalleryProps) {
 
 							{/* Embla Carousel - Glissement fluide natif */}
 							<div
-								className="absolute inset-0 overflow-hidden touch-pan-y"
+								className="absolute inset-0 overflow-hidden touch-pan-x"
 								ref={emblaRef}
 							>
 								<div className="flex h-full" ref={emblaContainerRef}>
